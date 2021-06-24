825e750b13bdfcec365a912d109525bfdaff653d
Shimon <shimon8090@gmail.com>
2021-06-24 18:53:33 +0300

    menu-items updated with navigation using withRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import "./App.css";
+import { Switch, Route, Link } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component.jsx";
+
+const HatsPage = (props) => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
+const TopicList = () => (
+  <div>
+    <h1>Topic List Page</h1>
+  </div>
+);
+
+const TopicDetail = () => (
+  <div>
+    <h1>Topic Detail Page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop/hats" component={HatsPage} />
+        <Route path="/topics" component={TopicList} />
+        <Route path="/topic/:topicId" component={TopicDetail} />
+      </Switch>
     </div>
   );
 }
